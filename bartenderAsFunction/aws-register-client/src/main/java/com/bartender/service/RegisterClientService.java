9253b2494b1a7be811ab5f8b0c976abb1b94bcb4
Fabian Gutierrez <fagossa@gmail.com>
2019-03-10 18:26:32 +0100

    Add the aws-iot sdk as a dependency

--- bartenderAsFunction/aws-register-client/src/main/java/com/bartender/service/RegisterClientService.java
+++ bartenderAsFunction/aws-register-client/src/main/java/com/bartender/service/RegisterClientService.java
@@ -4,21 +4,25 @@ import com.bartender.dao.RegisterClientRepository;
 import com.bartender.model.DrunkClient;
 import com.bartender.model.Json;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 import java.util.UUID;
 
 public class RegisterClientService {
 
+    private static final Logger LOG = LogManager.getLogger(RegisterClientService.class);
+
     private RegisterClientRepository registerClientRepository;
+    private final String policyDrunkClient;
 
     public RegisterClientService(RegisterClientRepository registerClientRepository) {
         this.registerClientRepository = registerClientRepository;
+        this.policyDrunkClient = System.getenv("POLICY_DRUNK_CLIENT");
     }
 
     public DrunkClient handleInput(Map<String, Object> input) {
-        final String policyDrunkClient = System.getenv("POLICY_DRUNK_CLIENT");
-
         DrunkClient drunkClient = Json.serializer().mapOrError(input, DrunkClient.class);
         if (StringUtils.isEmpty(drunkClient.getId())) {
             drunkClient.setId(UUID.randomUUID().toString());
