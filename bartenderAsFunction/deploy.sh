7473605389b3101ac3a044a0a6f407ef83393b89
Fabian Gutierrez <fagossa@gmail.com>
2019-03-14 22:00:33 +0100

    improvements in 'deploy.sh'

--- bartenderAsFunction/deploy.sh
+++ bartenderAsFunction/deploy.sh
@@ -10,18 +10,25 @@ for folder in bin/*;
 do
 echo $folder
 lambda_name=`basename $folder | sed 's/{//' | sed 's/}//'`
-echo $lambda_name
-zip bin/$lambda_name.zip bin/$lambda_name
+echo "Using: $lambda_name"
 
-aws s3api put-object --bucket handsonbartender --key $P_USER/$lambda_name.zip --body bin/$lambda_name.zip --profile epf
+aws s3api put-object \
+  --bucket handsonbartender \
+  --key $P_USER/$lambda_name \
+  --body bin/$lambda_name \
+  --profile epf
 
 done
-
 
 ## we omit https://docs.aws.amazon.com/cli/latest/reference/cloudformation/package.html and we do it by hand
 ## because it is more convenient for the workshop
 echo "deploying...."
 
-sam deploy --template-file ./sam.yml --stack-name $P_USER-bartender-sam-deploy --parameter-overrides User=$P_USER --capabilities CAPABILITY_IAM --profile epf
+aws cloudformation deploy \
+  --template-file ./sam.yml \
+  --stack-name $P_USER-bartender-sam-deploy \
+  --parameter-overrides User=$P_USER \
+  --capabilities CAPABILITY_IAM \
+  --profile epf
 
-rm bin/*.zip
+# rm bin/*.jar
